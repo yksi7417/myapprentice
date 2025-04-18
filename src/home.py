985fbf4442dcfc542719ce6763e12cfa54a48fb3
Anthony Si <yatkasi@gmail.com>
2025-04-18 13:25:50 -0400

    initial files

--- src/home.py
+++ src/home.py
@@ -1,3 +1,16 @@
 import streamlit as st
 from streamlit.navigation.page import StreamlitPage
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")
+    page_dir = "./pages"
+    pages: list[StreamlitPage] = [
+        st.Page(f"{page_dir}/introduction.py", title="Introduction", icon="🏠"),
+    ]
+    pages.append(st.Page(f"{page_dir}/acknowledgement.py", title="Acknowledgement", icon="📝"))
+    pg = st.navigation(pages, position="sidebar", expanded=len(pages) > 1)
+    pg.run()
